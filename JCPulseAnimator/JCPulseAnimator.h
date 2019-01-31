90aee740ddd30bd012f6a972ff9b841a74d7ac5c
Juan Curti <juan.curti@mediamonks.com>
2019-01-31 12:30:07 +0100

    First Commit

---
+++ JCPulseAnimator/JCPulseAnimator.h
@@ -0,0 +1,19 @@
+//
+//  JCPulseAnimator.h
+//  JCPulseAnimator
+//
+//  Created by Juan Curti on 31/01/2019.
+//  Copyright © 2019 Juan Curti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JCPulseAnimator.
+FOUNDATION_EXPORT double JCPulseAnimatorVersionNumber;
+
+//! Project version string for JCPulseAnimator.
+FOUNDATION_EXPORT const unsigned char JCPulseAnimatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JCPulseAnimator/PublicHeader.h>
+
+
---
+++ JCPulseAnimator/JCPulseShapeLayer.swift
@@ -0,0 +1,33 @@
+//
+//  JCPulseShapeLayer.swift
+//  JCPulseAnimator
+//
+//  Created by Juan Curti on 31/01/2019.
+//  Copyright © 2019 Juan Curti. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+internal class JCPulseShapeLayer: CAShapeLayer {
+    private var strokingColor: UIColor
+    private var fillingColor: UIColor
+    private var customLineWidth: CGFloat
+    
+    init(withStrokeColor strokeColor: UIColor = UIColor.black, fillColor: UIColor = UIColor.clear, lineWidth: CGFloat = 3.0) {
+        self.strokingColor = strokeColor
+        self.fillingColor = fillColor
+        self.customLineWidth = lineWidth
+        
+        super.init()
+        
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+//https://github.com/jmcur/JCPulseAnimator.git
