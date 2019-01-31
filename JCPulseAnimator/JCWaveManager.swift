849ae60bcf385ecd84953d0b81abde0a09d8c83f
Juan Curti <juanmacur22@gmail.com>
2019-01-31 13:42:48 +0100

    Add JCWaveManager

---
+++ JCPulseAnimator/JCWaveManager.swift
@@ -0,0 +1,14 @@
+//
+//  JCWave.swift
+//  JCPulseAnimator
+//
+//  Created by Juan Curti on 31/01/2019.
+//  Copyright © 2019 Juan Curti. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public class JCWaveManager {
+    
+}
