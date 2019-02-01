d544c67bf044fa83507e9ac8d5021316cf044806
Juan Curti <juanmacur22@gmail.com>
2019-02-01 15:15:24 +0100

    Make public to the end user the methods to start and stop animations, and to change the pulse frequency in NSInteger

--- JCPulseAnimator/JCPulseView.swift
+++ JCPulseAnimator/JCPulseView.swift
@@ -36,6 +36,18 @@ public class JCPulseView: UIView, JCDisplayLinkManagerDelegate {
         self.displayLinkManager.delegate = self
     }
     
+    func startAnimation() {
+        self.displayLinkManager.startDisplayLink()
+    }
+    
+    func stopAnimation() {
+        self.displayLinkManager.stopDisplayLink()
+    }
+    
+    public func changePulseFrequency(withValue value: NSInteger) {
+        self.displayLinkManager.setFrequency(frequency: value)
+    }
+    
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
